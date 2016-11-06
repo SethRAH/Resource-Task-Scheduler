67bacdf8208faae3b9c88dde1df0d4386772ac15
SethRAH <srahare@gmail.com>
2016-11-06 12:46:59 -0500

    Initial Add
    - Initial add to github.
    - Current state is some html mock up where I started to add vue.js
    functionality to manage resources (first couple of resources in the
    resource list are still hard coded)

---
+++ app/daily-resource-scheduler.js
@@ -0,0 +1,49 @@
+var scheduler = new Vue({
+
+    // The parent DOM node
+    el: '#main-content',
+
+    //Model
+    data: {
+        resources: [],
+        tasks: []
+    },
+
+    methods: {
+        addResource: function(){
+            var oldID = 0;
+            if(this.resources.length > 0){
+                //Get current max ID
+                for(var i = 0; i < this.resources.length; i++){
+                    if(this.resources[i].id > oldID){
+                        var oldID = this.resources[i].id;
+                    }
+                }
+            }
+
+            var newID = oldID + 1;
+
+            this.resources.push({
+                id: newID,
+                name: 'Resource'+newID,
+                qualifiers:[false, false, false]
+            });
+        },
+        removeResource: function(id){
+            //find first instance in array with id
+            var found = false;
+            var foundIndex = -1;
+            for(var i = 0; i < this.resources.length && !found; i++){
+                if(this.resources[i].id === id){
+                    found = true;
+                    foundIndex = i;
+                }
+            }
+            
+            //If resource was found, remove it
+            if(foundIndex > -1){
+                this.resources.splice(foundIndex, 1);
+            }
+        }
+    }
+});
